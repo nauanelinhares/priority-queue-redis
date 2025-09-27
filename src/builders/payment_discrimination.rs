d8c340f233924749d9457a9badea9ddfe8c57c63
nauanelinhares <nauane.linhares@gmail.com>
2025-09-27 11:20:35 -0300

    ✨ feat: add PaymentDiscriminationBuilder for payment handling

--- src/builders/payment_discrimination.rs
+++ src/builders/payment_discrimination.rs
@@ -18,3 +18,35 @@ pub fn build_payment_discrimination() -> PaymentDiscriminator {
         additional_paid,
     }
 }
+
+pub struct PaymentDiscriminationBuilder {
+    pub payment_discrimination: PaymentDiscriminator,
+}
+
+impl PaymentDiscriminationBuilder {
+    pub fn new() -> Self {
+        Self {
+            payment_discrimination: build_payment_discrimination(),
+        }
+    }
+    pub fn with_total_amount_paid(mut self, total_amount_paid: i64) -> Self {
+        self.payment_discrimination.total_amount_paid = total_amount_paid;
+        self
+    }
+    pub fn with_lost_discounts_paid(mut self, lost_discounts_paid: i64) -> Self {
+        self.payment_discrimination.lost_discounts_paid = lost_discounts_paid;
+        self
+    }
+    pub fn with_fine_amount_paid(mut self, fine_amount_paid: i64) -> Self {
+        self.payment_discrimination.fine_amount_paid = fine_amount_paid;
+        self
+    }
+    pub fn with_interest_amount_paid(mut self, interest_amount_paid: i64) -> Self {
+        self.payment_discrimination.interest_amount_paid = interest_amount_paid;
+        self
+    }
+    pub fn with_additional_paid(mut self, additional_paid: i64) -> Self {
+        self.payment_discrimination.additional_paid = additional_paid;
+        self
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 mod builders;
 mod structs;
 
-use builders::charge::{build_charge, ChargeBuilder};
+use builders::charge::ChargeBuilder;
 use serde_json;
 
-use crate::structs::charge::UNPAID;
+use crate::{
+    builders::payment_discrimination::PaymentDiscriminationBuilder, structs::charge::UNPAID,
+};
 
 fn main() {
-    let charge = ChargeBuilder::new().with_payment_status(UNPAID);
+    let payment_discrimination = PaymentDiscriminationBuilder::new()
+        .with_total_amount_paid(0)
+        .with_lost_discounts_paid(0)
+        .with_fine_amount_paid(0)
+        .with_interest_amount_paid(0)
+        .with_additional_paid(0)
+        .payment_discrimination;
+
+    let charge = ChargeBuilder::new()
+        .with_payment_status(UNPAID)
+        .with_payment_discriminator(payment_discrimination);
 
     match serde_json::to_string_pretty(&charge.charge) {
         Ok(json) => println!("{}", json),
