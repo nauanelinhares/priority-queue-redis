57867dc7ffa343d90f0a6303701b4d257aecec93
nauanelinhares <nauane.linhares@gmail.com>
2025-09-27 11:11:18 -0300

    chore: implement functions on charge builder

--- src/builders/charge.rs
+++ src/builders/charge.rs
@@ -1,7 +1,9 @@
 use crate::builders::payment_discrimination::build_payment_discrimination;
-use crate::structs::charge::{Charge, PAID, PARTIALLY_PAID, UNPAID};
+use crate::structs::charge::{
+    Charge, PaymentDiscriminator, PaymentStatus, PAID, PARTIALLY_PAID, UNPAID,
+};
 use chrono::Utc;
-use rand::seq::{IndexedRandom, SliceRandom};
+use rand::seq::IndexedRandom;
 use uuid::Uuid;
 
 pub fn build_charge() -> Charge {
@@ -23,4 +25,27 @@ pub fn build_charge() -> Charge {
 
 pub fn generate_random_uuid() -> Uuid {
     Uuid::new_v4()
+}
+
+pub struct ChargeBuilder {
+    pub charge: Charge,
+}
+
+impl ChargeBuilder {
+    pub fn new() -> Self {
+        Self {
+            charge: build_charge(),
+        }
+    }
+    pub fn with_payment_status(mut self, payment_status: PaymentStatus) -> Self {
+        self.charge.payment_status = payment_status;
+        self
+    }
+    pub fn with_payment_discriminator(
+        mut self,
+        payment_discriminator: PaymentDiscriminator,
+    ) -> Self {
+        self.charge.payment_discriminator = payment_discriminator;
+        self
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 mod builders;
 mod structs;
 
-use builders::charge::build_charge;
+use builders::charge::{build_charge, ChargeBuilder};
 use serde_json;
 
+use crate::structs::charge::UNPAID;
+
 fn main() {
-    let charge = build_charge();
-    
-    match serde_json::to_string_pretty(&charge) {
+    let charge = ChargeBuilder::new().with_payment_status(UNPAID);
+
+    match serde_json::to_string_pretty(&charge.charge) {
         Ok(json) => println!("{}", json),
         Err(e) => eprintln!("Erro ao serializar para JSON: {}", e),
     }
